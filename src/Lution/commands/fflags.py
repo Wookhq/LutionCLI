105ce85285271f0928a7940adf96856f441bce0c
cihp <itsjustchipoverhere@proton.me>
2025-07-01 12:50:50 +0700

    add the add command

--- src/Lution/commands/fflags.py
+++ src/Lution/commands/fflags.py
@@ -7,5 +7,16 @@ def run(args):
         fflagscf = ReadSoberConfig("fflags")
         pretty = json.dumps(fflagscf,indent=4)
         print(pretty)
-    else:
-        print("Type help to see how to use fflag command")
+
+    if args.add is None or len(args.add) == 0 or len(args.add) == 1 : # if nothing retrun usage
+        print("Usage: --add [fflag] [Value]")
+        return
+    
+    if len(args.add) >= 2:
+        fflag = args.add[0]
+        value = args.add[1]
+        print(f"debug fflag: {fflag} = {value}")
+        return
+
+
+print("Type help to see how to use fflag command")
